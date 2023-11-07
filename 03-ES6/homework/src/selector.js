3814071949e7b09fa5075da71101f9d796af2b28
gastondandrea <gastondandrea@hotmail.com>
2023-11-07 16:57:19 -0300

    Resolviendo homework falta funcion traverseDomAndCollectElements

--- 03-ES6/homework/src/selector.js
+++ 03-ES6/homework/src/selector.js
@@ -1,14 +1,17 @@
+//
+
 var traverseDomAndCollectElements = function (matchFunc, startEl) {
   var resultSet = [];
 
   if (typeof startEl === "undefined") {
     startEl = document.body;
   }
-
+  
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+  return resultSet ;
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +19,17 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  let tipoSelector;
+  if(selector[0] === '#'){ 
+    tipoSelector = 'id';
+  }else if(selector[0] === '.'){
+    tipoSelector = 'class';
+  }else if(selector.includes('.')){
+    tipoSelector = 'tag.class';
+  }else{
+    tipoSelector = 'tag';
+  }
+  return tipoSelector;
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -27,16 +41,53 @@ var matchFunctionMaker = function (selector) {
   var selectorType = selectorTypeMatcher(selector);
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = function(elemento){
+      const newSelector = selector.slice(1);
+      if(elemento.id === newSelector){
+        return true;
+      }else{
+        return false;
+      }
+    }
   } else if (selectorType === "class") {
+    matchFunction = function(elemento){
+      const newSelector = selector.slice(1);
+      if(elemento.classList.contains(newSelector)){
+        return true;
+      }else{
+        return false;
+      }
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = function(elemento){
+      const newSelector = selector.split('.');
+      if(elemento.tagName.toLowerCase() === newSelector[0] && elemento.classList.contains(newSelector[1])){
+        return true;
+      }else{
+        return false;
+      }
+    }
   } else if (selectorType === "tag") {
+    matchFunction = function(elemento){
+      if(elemento.tagName.toLowerCase() === selector){
+        return true;
+      }else{
+        return false;
+      }
+    }
   }
   return matchFunction;
 };
 
+
+// funcion que recibe un selector del Dom.
 var $ = function (selector) {
   var elements;
   var selectorMatchFunc = matchFunctionMaker(selector);
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+//ejemplo de selector que recibe funcion de arriba
+$(".clasecualquiera");
+$("#idcualquiera");
+$("p");
