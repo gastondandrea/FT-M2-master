a9aaf40a802ddfba02e812c02cac1e6e688d9b7a
gastondandrea <gastondandrea@hotmail.com>
2023-11-14 15:13:13 -0300

    hw 05-bundlers

--- 05-Bundlers/homework/browser/app.js
+++ 05-Bundlers/homework/browser/app.js
@@ -1,25 +1,25 @@
-(function () {
-  var whiteboard = window.whiteboard;
-  var socket = window.io(window.location.origin);
 
-  socket.on("connect", function () {
-    console.log("Connected!");
-  });
+const whiteboard = require("./whiteboard.js");
+var socket = require("socket.io-client");
 
-  socket.on("load", function (strokes) {
-    strokes.forEach(function (stroke) {
-      var start = stroke.start;
-      var end = stroke.end;
-      var color = stroke.color;
-      whiteboard.draw(start, end, color, false);
-    });
-  });
+socket.on("connect", function () {
+  console.log("Connected!");
+});
 
-  socket.on("draw", function (start, end, color) {
+socket.on("load", function (strokes) {
+  strokes.forEach(function (stroke) {
+    var start = stroke.start;
+    var end = stroke.end;
+    var color = stroke.color;
     whiteboard.draw(start, end, color, false);
   });
+});
 
-  whiteboard.on("draw", function (start, end, color) {
-    socket.emit("draw", start, end, color);
-  });
-})();
+socket.on("draw", function (start, end, color) {
+  whiteboard.draw(start, end, color, false);
+});
+
+whiteboard.on("draw", function (start, end, color) {
+  socket.emit("draw", start, end, color);
+});
+
